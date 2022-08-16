5f441b77299c2f447249ed7dac69d971448dd9c6
James Henstridge <james@jamesh.id.au>
2022-08-16 22:44:10 +0800

    Add abstract class for representing calendars.

---
+++ calendar_check/calendar.py
@@ -0,0 +1,28 @@
+import datetime
+import typing
+
+
+class Event:
+    id: str
+    link: str
+    start: datetime.datetime
+    duration: datetime.timedelta
+    summary: str
+    description: str
+
+    def __init__(self, id, link, start, duration, summary, description):
+        self.id = id
+        self.link = link
+        self.start = start
+        self.duration = duration
+        self.summary = summary
+        self.description = description
+
+    def __lt__(self, other: 'Event') -> bool:
+        return self.start < other.start or (self.start == other.start and self.duration < other.duration)
+
+
+class Calendar:
+
+    def events(self, start: datetime.datetime, end: datetime.datetime) -> typing.List[Event]:
+        raise NotImplementedError
